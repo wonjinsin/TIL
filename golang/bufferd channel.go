bcb8d9e98e838e5deb243d0e910039a4e4124fe9
Wonjinsin <slgktn92@gmail.com>
2025-02-25 18:58:26 +0900

    add. 20250225

--- golang/bufferd channel.go
+++ golang/bufferd channel.go
@@ -12,7 +12,7 @@ func unbuffered() {
 	done := make(chan bool)
 
 	go func() {
-		done <- true                  // done을 누가 수신 하기전에 block
+		done <- true                  // done을 누가 수신 하기전에 block, block
 		print("print after use done") // done 수신되고 print (5초 기다림)
 	}()
 	time.Sleep(5 * time.Second)
@@ -29,7 +29,7 @@ func buffered() {
 	done := make(chan bool, 1)
 
 	go func() {
-		done <- true                   // done을 누가 수신 하든말든 다음 동작함
+		done <- true                   // done을 누가 수신 하든말든 다음 동작함, non-blocking
 		print("print before use done") // 안기다리고 바로 print
 	}()
 	time.Sleep(5 * time.Second)
---
+++ golang/non-blocking2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+var id atomic.Int64
+
+func main() {
+	var signal = make(chan bool)
+
+	go func() {
+		for {
+			_, ok := <-signal
+			fmt.Println("Received signal", ok)
+			if !ok {
+				fmt.Println("Exit Worker")
+				return
+			}
+
+			for {
+				latest := id.Swap(0)
+				if latest == 0 {
+					break
+				}
+
+				fmt.Printf("ID is %d\n", latest)
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+
+	for i := int64(0); i < 40; i++ {
+		time.Sleep(100 * time.Millisecond)
+		id.Store(i + 1)
+
+		select {
+		case signal <- true:
+			fmt.Println("Sent signal")
+		// 이 default가 없으면, signal에 데이터를 넣는게 block이 되기 때문에,
+		// go routine안에 최상의 forloop가 다음으로 가기 전까지 위에있는 signal <- true가 계속 대기함
+		// 즉, non-blocking이 안됨
+		default:
+		}
+	}
+	close(signal)
+	time.Sleep(2 * time.Second)
+}
